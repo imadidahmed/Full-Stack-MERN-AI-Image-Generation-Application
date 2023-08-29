88a91405ce5a2fe91c50e5f02ad60ab0d0f1dbe8
IMAD ID AHMED <imad.idahmed.1@gmail.com>
2023-08-29 17:53:31 +0100

    first commit

---
+++ server/routes/postRoutes.js
@@ -0,0 +1,43 @@
+import express from "express";
+import * as dotenv from "dotenv";
+import { v2 as cloudinary } from "cloudinary";
+
+import Post from "../mongodb/models/post.js";
+
+dotenv.config();
+
+const router = express.Router();
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+router.route("/").get(async (req, res) => {
+    try {
+        const posts = await Post.find({});
+        res.json({success: true, data:posts});    
+    } catch (error) {
+        res.json({success: false, message: error });
+    }
+    
+});
+
+router.route("/").post(async (req, res) => {
+  try {
+    const { name, prompt, photo } = req.body;
+    const photoURL = await cloudinary.uploader.upload(photo);
+
+    const newPost = Post.create({
+      name,
+      prompt,
+      photo: photoURL.url,
+    });
+    res.json({ success: true, data: newPost });
+  } catch (error) {
+    res.json({ success: false, message: error });
+  }
+});
+
+export default router;
